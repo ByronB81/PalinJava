4f4e0be0b140b8082ec4e4c1c77ed65f0d2a8004
Byron <byronblocker@gmail.com>
2017-05-22 12:56:11 -0700

    add assertion

--- src/test/PalinJavaTest.java
+++ src/test/PalinJavaTest.java
@@ -1,2 +1,16 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalinJavaTest {
+
+  @Test
+  public void runPalinJava_returnString_ArrayList() {
+    Palindrome testPalindrome = new Palindrom();
+    List<Object> expectedOutput = new ArrayList<Object>();
+    expectedOutput.add("ab");
+    assertEquals(expectedOutput, testPalindrome.runPalinJava("ab"));
+  }
+
+}
